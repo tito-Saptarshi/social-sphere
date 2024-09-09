2fce03ff4ae91c6b339a0dde26d7b66587d5ac70
tito-Saptarshi <saptarshi.tito.20@gmail.com>
2024-09-09 19:42:53 +0530

    redirect -c1

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,8 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+
+import { unstable_noStore as noStore } from "next/cache";
+
+export const GET = () => {
+  noStore();
+  handleAuth();
+};
